fa63cc03a8f15897b57776aeb5dac117f530a97f
dimi-fn <dimitris_fanis@protonmail.com>
2022-02-21 00:48:45 +0200

    updated changes

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const hamburgerButton = document.getElementById("hamburger")
+// <button class="hamburger" id="hamburger">
+const hamburgerButton = document.getElementById("hamburger") 
 
-const navList = document.getElementById("nav-list")
+const navList = document.getElementById("nav-list") // <ul id="nav-list">
 
 function toggleButton(){
     navList.classList.toggle("show")
 }
 
+// when you click the hamburger button, toggle it by revoking the toggleButton function above
 hamburgerButton.addEventListener("click", toggleButton)
