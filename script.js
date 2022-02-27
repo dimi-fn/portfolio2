dea56ccdd227821c725a7990c8ddbc176bbd5aa5
dimi-fn <dimitris_fanis@protonmail.com>
2022-02-27 22:35:23 +0200

    updated contact form

--- script.js
+++ script.js
@@ -7,5 +7,79 @@ function toggleButton(){
     navList.classList.toggle("show")
 }
 
-// when you click the hamburger button, toggle it by revoking the toggleButton function above
-hamburgerButton.addEventListener("click", toggleButton)
+/* Without async function and without time delay for the message "Sending..."*/
+// // Contact Form Validation 
+// function validateForm() {
+//     var name =  document.getElementById('name').value;
+//     if (name == "") {
+//         document.querySelector('.status').innerHTML = "Name cannot be empty!";
+//         return false;
+//     }
+//     var email =  document.getElementById('email').value;
+//     if (email == "") {
+//         document.querySelector('.status').innerHTML = "Email cannot be empty!";
+//         return false;
+//     } else {
+//         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//         if(!re.test(email)){
+//             document.querySelector('.status').innerHTML = "Email format invalid!";
+//             return false;
+//         }
+//     }
+//     var subject =  document.getElementById('subject').value;
+//     if (subject == "") {
+//         document.querySelector('.status').innerHTML = "Subject cannot be empty!";
+//         return false;
+//     }
+//     var message =  document.getElementById('message').value;
+//     if (message == "") {
+//         document.querySelector('.status').innerHTML = "Message cannot be empty!";
+//         return false;
+//     }
+    
+//     document.querySelector('.status').innerHTML = "Sending...";
+   
+   
+//     document.getElementById('contact-form').submit();
+
+//   };
+
+
+// Using asynchronous function in order to use the setTimeout function before "submit"
+const validateForm = async() =>{
+        var name =  document.getElementById('name').value;
+        if (name == "") {
+            document.querySelector('.status').innerHTML = "Name cannot be empty!";
+            return false;
+        }
+        var email =  document.getElementById('email').value;
+        if (email == "") {
+            document.querySelector('.status').innerHTML = "Email cannot be empty!";
+            return false;
+        } else {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if(!re.test(email)){
+                document.querySelector('.status').innerHTML = "Email format invalid!";
+                return false;
+            }
+        }
+        var subject =  document.getElementById('subject').value;
+        if (subject == "") {
+            document.querySelector('.status').innerHTML = "Subject cannot be empty!";
+            return false;
+        }
+        var message =  document.getElementById('message').value;
+        if (message == "") {
+            document.querySelector('.status').innerHTML = "Message cannot be empty!";
+            return false;
+        }
+        
+        document.querySelector('.status').innerHTML = "Sending...";
+       
+        // wait 1000ms before submitting (in order to display the message "Sending")
+        await new Promise(resolve => setTimeout(resolve, 1000)); 
+
+        document.getElementById('contact-form').submit();
+          
+      };
+    
