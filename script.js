312146986b219684bd5f659a7243707e8b36018b
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-06 18:10:49 +0200

    updated script.js putting const instead of var

--- script.js
+++ script.js
@@ -10,69 +10,31 @@ function toggleButton(){
     navList.classList.toggle("show")
 };
 
-/* Without async function and without time delay for the message "Sending..."*/
-// // Contact Form Validation 
-// function validateForm() {
-//     var name =  document.getElementById('name').value;
-//     if (name == "") {
-//         document.querySelector('.status').innerHTML = "Name cannot be empty!";
-//         return false;
-//     }
-//     var email =  document.getElementById('email').value;
-//     if (email == "") {
-//         document.querySelector('.status').innerHTML = "Email cannot be empty!";
-//         return false;
-//     } else {
-//         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//         if(!re.test(email)){
-//             document.querySelector('.status').innerHTML = "Email format invalid!";
-//             return false;
-//         }
-//     }
-//     var subject =  document.getElementById('subject').value;
-//     if (subject == "") {
-//         document.querySelector('.status').innerHTML = "Subject cannot be empty!";
-//         return false;
-//     }
-//     var message =  document.getElementById('message').value;
-//     if (message == "") {
-//         document.querySelector('.status').innerHTML = "Message cannot be empty!";
-//         return false;
-//     }
-    
-//     document.querySelector('.status').innerHTML = "Sending...";
-   
-   
-//     document.getElementById('contact-form').submit();
-
-//   };
-
-
-// Using asynchronous function in order to use the setTimeout function before "submit"
+/* Input validation for contact form */
 const validateForm = async() =>{
         document.querySelector('.status').style.color = "red";
-        var name =  document.getElementById('name').value;
+        const name =  document.getElementById('name').value;
         if (name == "") {
             document.querySelector('.status').innerHTML = "Name cannot be empty!";
             return false;
         }
-        var email =  document.getElementById('email').value;
+        const email =  document.getElementById('email').value;
         if (email == "") {
             document.querySelector('.status').innerHTML = "Email cannot be empty!";
             return false;
         } else {
-            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             if(!re.test(email)){
                 document.querySelector('.status').innerHTML = "Email format invalid!";
                 return false;
             }
         }
-        var subject =  document.getElementById('subject').value;
+        const subject =  document.getElementById('subject').value;
         if (subject == "") {
             document.querySelector('.status').innerHTML = "Subject cannot be empty!";
             return false;
         }
-        var message =  document.getElementById('message').value;
+        const message =  document.getElementById('message').value;
         if (message == "") {
             document.querySelector('.status').innerHTML = "Message cannot be empty!";
             return false;
@@ -83,6 +45,7 @@ const validateForm = async() =>{
         document.querySelector('.status').style.fontWeight  = "bold";
        
         // wait 1000ms before submitting (in order to display the message "Sending")
+        // Using asynchronous function in order to use the setTimeout function before "submit"
         await new Promise(resolve => setTimeout(resolve, 1000)); 
 
         document.getElementById('contact-form').submit();
